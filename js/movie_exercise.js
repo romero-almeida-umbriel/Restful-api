b98a96a849e4fb5e74021e14b05c179192569a4f
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-06-06 16:53:33 -0500

    rendered movies

--- js/movie_exercise.js
+++ js/movie_exercise.js
@@ -1,11 +1,25 @@
 "use strict";
-(function(){
+(function () {
+    const URL = 'https://expensive-narrow-license.glitch.me/movies';
+    //Function to get all movies
+    let allMovies = () => fetch(URL).then(res => res.json());
+    //Function to render movies
+    let renderMovies = () => {
+         allMovies().then((data) => {
+             let movieCard = data.map(movie => {
+                 return `<div>
+                    <h2>Title: ${movie.title}</h2>
+                    <h3>Director: ${movie.director}</h3>
+                    <h3>Genre: ${movie.genre}</h3>
+                    <h3>Rating: ${movie.rating}</h3>
+                    </div>`
+             })
+             console.log(movieCard);
+             $("#movies").html(movieCard);
+         })
+
+    };
+    renderMovies();
 
 
-
-
-
-
-
-
-}())
+}());
