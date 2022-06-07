c3e4fb6ed2c6f917547c66bd1936482c030a1574
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-07 11:09:50 -0500

    luis  project update

--- js/movie_exercise.js
+++ js/movie_exercise.js
@@ -9,9 +9,8 @@
              let movieCard = data.map(movie => {
                  return `<div>
                     <h3>Title: ${movie.title}</h3>
-                    <h4>Director: ${movie.director}</h4>
-                    <h4>Genre: ${movie.genre}</h4>
-                    <h4>Rating${movie.rating}</h4>
+                    <h4>Rating: ${movie.rating}</h4>
+                    <button id="edit">Edit</button>
                     </div>`
              })
              console.log(movieCard);
@@ -20,6 +19,44 @@
 
     };
     renderMovies();
+
+    const addMovie = (movieObj) => {
+        let options = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(movieObj)
+        }
+        return fetch(URL, options).then(resp => resp.json()).then(result => console.log(result))
+    }
+
+    const editMovie = (movie) => {
+        let options = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movie)
+        }
+        return fetch(`${URL}, ${options}`).then(resp => resp.json())
+    }
+
+    $('#addMovie').click(function(e) {
+        e.preventDefault();
+        let addNewMovie = {
+            title: $('#movieTitle').val(),
+            rating: $('#movieRating').val(),
+        }
+        addMovie(addNewMovie).then(resp => renderMovies())
+    })
+
+    $('#edit').click(function() {
+        let editMovie = {
+
+        }
+
+    })
 
 
 }());
